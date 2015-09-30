4a2c3685a3758293081a0d884275edc42428fe17
mrcportillo <marcossportillo@gmail.com>
2015-09-30 17:31:14 -0300

    prueba getApi

--- parseServerCode/parseServerCode/cloud/main.js
+++ parseServerCode/parseServerCode/cloud/main.js
@@ -4,3 +4,17 @@
 Parse.Cloud.define("hello", function(request, response) {
   response.success("Hello world!");
 });
+
+var client = require('cloud/apiClient.js');
+
+Parse.Cloud.define("test", function(request, response) {
+  client.getEsculturas({
+    lat: "10",
+    lon: "10"
+  }).then(funtion(httpResponse) {
+    response.success("success client call");
+  }, function(httpResponse) {
+    console.error(httpResponse);
+    response.error("Uh oh, something went wrong in client call");
+  });
+})
